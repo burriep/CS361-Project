2a91d37003aac0c168e77ef8769d7e7b9ee4ea3a
Paul Burrie <burriep@gmail.com>
2016-02-28 18:38:57 -0600

    Fixed lab5 driver

--- src/lab5/Driver.java
+++ src/lab5/Driver.java
@@ -1,15 +1,12 @@
 package lab5;
 
-import java.util.*;
-
 public class Driver {
-	public static void main(String[] args){
-		Scanner stdIn = new Scanner(System.in);
-		for (int i=0; i<4; i++) {
-			System.out.println("Test " + i+1);
-			private ATM myATM = new ATM();
+	public static void main(String[] args) {
+		for (int i = 0; i < 4; i++) {
+			System.out.println("Test " + (i + 1));
+			ATM myATM = new ATM();
 			myATM.start();
-			System.out.println("End of Test " + i+1);
+			System.out.println("End of Test " + (i + 1));
 		}
 	}
 }
