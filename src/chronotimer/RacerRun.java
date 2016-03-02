85b71042972b979dbc971a970a4eef9b2b23481d
Paul Burrie <burriep@gmail.com>
2016-03-02 14:14:22 -0600

    Clarifying and adding docs

--- src/chronotimer/RacerRun.java
+++ src/chronotimer/RacerRun.java
@@ -1,5 +1,64 @@
 package chronotimer;
 
 public class RacerRun {
+	private String startTime;
+	private String endTime;
+	private Racer racer;
 
+	/**
+	 * Create a new RacerRun for Racer r with no start or end time.
+	 * 
+	 * @param r
+	 */
+	public RacerRun(Racer r) {
+		racer = r;
+	}
+
+	/**
+	 * Return the racer associated with this RacerRun.
+	 * 
+	 * @return the racer associated with this RacerRun
+	 */
+	public Racer getRacer() {
+		return racer;
+	}
+
+	/**
+	 * Set the start time for this RacerRun
+	 * 
+	 * @param time
+	 *            - the start time. Must be in the format HH:MM:SS.ss
+	 */
+	public void setStartTime(String time) {
+		startTime = time;
+	}
+
+	/**
+	 * Return the start time associated with this RacerRun or NULL if the start
+	 * time has not been set.
+	 * 
+	 * @return the start time (in the format HH:MM:SS.ss) or NULL if the start
+	 *         time has not been added.
+	 */
+	public String getStartTime() {
+		return startTime;
+	}
+
+	/**
+	 * Set the end time for this RacerRun
+	 * 
+	 * @param time
+	 *            - the end time. Must be in the format HH:MM:SS.ss
+	 */
+	public void setEndTime(String time) {
+		endTime = time;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public double getElapsedTime() {
+		return Timer.getDifference(startTime, endTime);
+	}
 }
--- src/chronotimer/Timer.java
+++ src/chronotimer/Timer.java
@@ -1,12 +1,51 @@
 package chronotimer;
 
 public class Timer {
-	public int getTime() {
-		// TODO: return type should probably be different.
-		return 0;
+	String currentTime;
+
+	/**
+	 * Returns the current time from this Timer
+	 * 
+	 * @return current time as a string in the format HH:MM:SS.ss
+	 */
+	public String getTime() {
+		return currentTime;
 	}
 
-	public void setTime(int t) {
-		// TODO: parameter type should probably be different.
+	/**
+	 * Set the current time for this Timer
+	 * 
+	 * @param time
+	 *            - the current time. Must be in the format HH:MM:SS.ss
+	 */
+	public void setTime(String time) {
+		if (validateTime(time)) {
+			currentTime = time;
+		}
+	}
+
+	/**
+	 * Get the difference between 2 times.
+	 * 
+	 * @param start
+	 *            - the start time. Must be in the format HH:MM:SS.ss
+	 * @param end
+	 *            - the end time. Must be in the format HH:MM:SS.ss
+	 * @return (end - start) in seconds, as a double.
+	 */
+	public static double getDifference(String start, String end) {
+		if (validateTime(start) && validateTime(end)) {
+			// TODO
+		}
+		return 0.0;
+	}
+
+	/**
+	 * Validates a string to determine if it follows the time format of
+	 * HH:MM:SS.ss
+	 */
+	public static boolean validateTime(String t) {
+		// TODO
+		return false;
 	}
 }
