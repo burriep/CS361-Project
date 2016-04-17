da5529b5764ea38ab2148dc09f84926ebec8553f
Paul Burrie <burriep@gmail.com>
2016-04-17 17:15:56 -0500

    Made Channel tests more concise and thorough

--- src/tests/ChannelTest.java
+++ src/tests/ChannelTest.java
@@ -1,60 +1,53 @@
 package tests;
 
 import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
 import org.junit.Test;
-
 import chronotimer.*;
 
 public class ChannelTest {
-
 	@Test
-	public void testOneChannel() {
-		Channel s = new Channel();
-		assertEquals(false, s.isEnabled());
-		s.toggleState();
-		assertEquals(true, s.isEnabled());
-		s.toggleState();
-		assertEquals(false, s.isEnabled());
+	public void testInitialization() {
+		Channel c = new Channel();
+		assertEquals(false, c.isEnabled());
+		assertEquals(false, c.isConnected());
+		assertEquals(null, c.getSensor());
+		assertEquals(null, c.getButton());
 	}
 
 	@Test
-	public void testMultipleChannels() {
-		ArrayList<Channel> channels = new ArrayList<Channel>(12);
-		for (int i = 0; i < channels.size(); i++) {
-			assertEquals(false, channels.get(i).isEnabled());
-		}
-
-		for (int i = 0; i < channels.size(); i++) {
-			if (i % 2 == 0) {
-				channels.get(i).toggleState();
-			}
-		}
-		for (int i = 0; i < channels.size(); i++) {
-			if (i % 2 == 0) {
-				assertEquals(true, channels.get(i).isEnabled());
-			} else {
-				assertEquals(false, channels.get(i).isEnabled());
-			}
-		}
+	public void testState() {
+		Channel c = new Channel();
+		c.toggleState();
+		assertEquals(true, c.isEnabled());
+		c.toggleState();
+		assertEquals(false, c.isEnabled());
 	}
 
-	@Test
-	public void testZeroChannels() {
-		ArrayList<Channel> channels = new ArrayList<Channel>(12);
-		for (int i = 0; i < channels.size(); i++) {
-			assertEquals(false, channels.get(i).isEnabled());
-		}
-	}
-	
 	@Test
 	public void testSensorConnection() {
-		ArrayList<Channel> channels = new ArrayList<Channel>(12);
-		assertEquals(false, channels.get(0).isConnected());
-		channels.get(0).connect(new Sensor(SensorType.EYE));
-		assertEquals(true, channels.get(0).isConnected());
-	}
+		Sensor s1 = new Sensor(SensorType.EYE);
+		Sensor s2 = new Sensor(SensorType.PUSH);
+		Channel c = new Channel();
 
+		c.connect(s1);
+		assertEquals(true, c.isConnected());
+		assertEquals(s1, c.getSensor());
+		c.disconnect();
+		assertEquals(false, c.isConnected());
+
+		c.connectButton(s2);
+		assertEquals(true, c.isConnected());
+		assertEquals(s2, c.getButton());
+		c.disconnectButton();
+		assertEquals(false, c.isConnected());
+
+		c.connect(s1);
+		c.connectButton(s2);
+		assertEquals(true, c.isConnected());
+		assertEquals(s1, c.getSensor());
+		assertEquals(s2, c.getButton());
+		c.disconnect();
+		c.disconnectButton();
+		assertEquals(false, c.isConnected());
+	}
 }
