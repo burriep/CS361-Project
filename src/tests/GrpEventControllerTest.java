389a166d4069b59c3feec2033333c84a013aef75
plindell <plindell@uwm.edu>
2016-05-03 21:58:27 -0500

    GrpEventControllerTest update
    These tests all work now.

--- src/tests/GrpEventControllerTest.java
+++ src/tests/GrpEventControllerTest.java
@@ -1,52 +1,74 @@
 package tests;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
+
 import chronotimer.*;
 
 public class GrpEventControllerTest {
 	@Test
 	public void testChannelTriggered() {
-		fail("Not yet implemented"); // TODO
+		Timer t = new Timer("11:00:00.00", false);
+		Run r = new Run();
+		GrpEventController ec = new GrpEventController(t, r);
+		ec.addRacer(123);
+		ec.channelTriggered(1);
+		ec.channelTriggered(2);
+		assertEquals(0, r.getQueuedRacers().size());
+		assertEquals(1, r.getData().size());
+		assertEquals(t.getTime(), r.getData().get(0).getStartTime());
+		assertEquals(t.getTime(), r.getData().get(0).getEndTime());
 	}
 
 	@Test
 	public void testAddRacer() {
-		fail("Not yet implemented"); // TODO
+		Timer t = new Timer("11:00:00.00", false);
+		Run r = new Run();
+		GrpEventController ec = new GrpEventController(t, r);
+		ec.addRacer(1);
+		ec.addRacer(2);
+		ec.addRacer(3);
+		ec.channelTriggered(1);
+		ec.channelTriggered(2);
+		ec.channelTriggered(2);
+		ec.channelTriggered(2);
+		assertEquals(3, r.getData().size());
+		assertEquals(1, r.getData().get(0).getRacer());
+		assertEquals(2, r.getData().get(1).getRacer());
+		assertEquals(3, r.getData().get(2).getRacer());
 	}
 
 	@Test
 	public void testEndRun() {
-		fail("Not yet implemented"); // TODO
+		Timer t = new Timer("11:00:00.00", false);
+		Run r = new Run();
+		GrpEventController ec = new GrpEventController(t, r);
+		ec.addRacer(1);
+		ec.channelTriggered(1);
+		ec.channelTriggered(2);
+		ec.endRun();
 	}
 
 	@Test
 	public void testGetRunningDisplay() {
-		fail("Not yet implemented"); // TODO
+		Timer t = new Timer("11:00:00.00", false);
+		Run r = new Run();
+		GrpEventController ec = new GrpEventController(t, r);
+		ec.addRacer(1);
+		ec.channelTriggered(1);
+		ec.channelTriggered(2);
+		assertEquals("0.0\n1 0.0 F\n", ec.getRunningDisplay());
 	}
 
-	@Test
-	public void testGrpEventController() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testEventController() {
-		fail("Not yet implemented"); // TODO
-	}
 
 	@Test
 	public void testClearRacer() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testSwapRacer() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testDnfRacer() {
-		fail("Not yet implemented"); // TODO
+		Timer t = new Timer("11:00:00.00", false);
+		Run r = new Run();
+		GrpEventController ec = new GrpEventController(t, r);
+		ec.addRacer(123);
+		ec.clearRacer(123);
+		assertEquals(0, r.getQueuedRacers().size());
 	}
 }
